cff79f4fbe43f258aefd4cc73c4283ecfd9461ba
unknown <wow1ghzx@gmail.com>
2019-02-27 09:51:29 +0200

    day 2

--- src/main/java/com/example/Bookstore1/web/BookController.java
+++ src/main/java/com/example/Bookstore1/web/BookController.java
@@ -1,15 +1,21 @@
 package com.example.Bookstore1.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore1.domain.Book;
 import com.example.Bookstore1.domain.BookRepository;
 import com.example.Bookstore1.domain.CategoryRepository;
+
+
 
 
 @Controller
@@ -47,6 +53,18 @@ public class BookController {
 
     return "editbook";
     }
+    
+    
+    //rest, search by id 
+    @RequestMapping(value="/books/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findStudentRest(@PathVariable("id") Long id) {	
+    	return repository.findById(id);
+    }    
+    //rest all books
+    @RequestMapping(value="/allbooks", method = RequestMethod.GET)
+    public @ResponseBody List<Book> studentListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
     
     
     
